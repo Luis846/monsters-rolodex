a0ba628d672e243643c66e000ba583c59e2abea6
Luis <luis.rivera846@gmail.com>
2020-02-01 19:13:37 -0500

    Updates

---
+++ precache-manifest.549b49de9e3740af8c6c752303551bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a1e2277e7c0085a05df6c170f31516",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84444a09025886a97313",
+    "url": "/monsters-rolodex/static/css/main.42c5cb5a.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84444a09025886a97313",
+    "url": "/monsters-rolodex/static/js/main.92f10251.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
